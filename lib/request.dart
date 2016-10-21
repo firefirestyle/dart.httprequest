4193c0f55c8e17d638c70d29f0fe20503d8a5fdb
kyorohiro <kyorohiro@gmail.com>
2016-10-21 14:59:32 +0900

    divide file

--- lib/request.dart
+++ lib/request.dart
@@ -3,5 +3,8 @@ library httprequest;
 import 'dart:async';
 import 'dart:typed_data';
 import 'dart:convert';
-
+import 'dart:math' as math;
 part 'src/request.dart';
+part 'src/urlencode.dart';
+part 'src/uuid.dart';
+part 'src/multipart.dart';
---
+++ lib/src/multipart.dart
@@ -0,0 +1,36 @@
+part of httprequest;
+
+class MultipartItem {
+  String name;
+  String fileName;
+  String contentType;
+  String base64src;
+
+  MultipartItem.fromBase64(this.name, this.fileName, this.contentType, this.base64src) {
+  }
+
+  String toString() {
+    StringBuffer buffer = new StringBuffer();
+    buffer.write("""Content-Disposition: form-data; name="${name}"; filename="${fileName}"\r\n""");
+    buffer.write("""Content-Type: "${contentType}"\r\n""");
+    buffer.write("""\r\n""");
+    buffer.write(base64src);
+    buffer.write("""\r\n""");
+  }
+}
+
+class Multipart {
+  List<int> bakeMultiPartFromBinary(List<MultipartItem> items) {
+      StringBuffer buffer = new StringBuffer();
+      String boundary = "--"+Uuid.createUUID().replaceAll("-","");
+      buffer.write("""Content-Type: multipart/form-data; boundary=${boundary}\r\n""");
+      buffer.write("""\r\n""");
+      buffer.write("""${boundary}\r\n""");
+      for(var item in items) {
+        buffer.write(item.toString());
+        buffer.write("""${boundary}\r\n""");
+      }
+      buffer.write("""\r\n""");
+      return UTF8.encode(buffer.toString());
+  }
+}
