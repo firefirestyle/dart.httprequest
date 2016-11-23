ff3690e0e1efae4765a071a69c957428b9c1a9f0
kyorohiro <kyorohiro@gmail.com>
2016-11-24 07:53:13 +0900

    ..

--- test/demo/demo.dart
+++ test/demo/demo.dart
@@ -1,2 +1,12 @@
 import 'package:firefirestyle.httprequest/request.dart';
 import 'package:firefirestyle.httprequest/request_io.dart';
+import 'dart:convert';
+main(List<String> args) async {
+    print("hello ${args}");
+    IONetBuilder builder = new IONetBuilder();
+    Requester requester = await builder.createRequester();
+    Response response = await requester.request("GET", "http://www.google.com");
+    print("status :: ${response.status}");
+    print("headers :: ${response.headers}");
+    print("body :: ${UTF8.decode(response.response.asUint8List(),allowMalformed: true)}");
+}
