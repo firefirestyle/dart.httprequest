98d3bdddaa3a99bc02e61e9a0fbced376842addb
kyorohiro <kyorohiro@gmail.com>
2016-12-20 03:28:39 +0900

    ..

--- lib/src/io.dart
+++ lib/src/io.dart
@@ -11,8 +11,13 @@ class IORequester extends Requester {
     if (headers == null) {
       headers = {};
     }
+    print("[A1]");
     io.HttpClient client = new io.HttpClient(context: io.SecurityContext.defaultContext);
+
+    print("[A2]");
     io.HttpClientRequest req = await client.openUrl(type, Uri.parse(url));
+
+    print("[A3]");
     for (String k in headers.keys) {
       req.headers.add(k, headers[k]);
     }
@@ -20,11 +25,16 @@ class IORequester extends Requester {
     if (data != null) {
       req.write(data);
     }
+    print("[A4]");
     io.HttpClientResponse res = await req.close();
+
+    print("[A5]");
     Map<String, List<String>> headerss = {};
     res.headers.forEach((String name, List<String> values) {
       headerss[name] = new List.from(values);
     });
+
+    print("[A6]");
     return new Response(res.statusCode, headerss, new typed.Uint8List.fromList(await res.single).buffer);
   }
 }
